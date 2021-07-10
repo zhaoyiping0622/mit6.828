705b17f660137d602f92425080d767613ac1d248
zhaoyiping <zhaoyiping0622@gmail.com>
2021-07-10 17:28:49 +0800

    finish Exercise 4

--- kern/pmap.c
+++ kern/pmap.c
@@ -225,11 +225,11 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
+	// changed in lab4
+  boot_map_region(kern_pgdir, KERNBASE, 0xFFFFFFFF-KERNBASE, 0, PTE_W);
 
 	// Initialize the SMP-related parts of the memory map
 	mem_init_mp();
-	// changed in lab4
-  boot_map_region(kern_pgdir, KERNBASE, 0xFFFFFFFF-KERNBASE, 0, PTE_W);
 
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
@@ -278,6 +278,8 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
+  for(int i=0;i<NCPU;i++)
+    boot_map_region(kern_pgdir, KSTACKTOP-i*(KSTKSIZE+KSTKGAP)-KSTKSIZE, KSTKSIZE, PADDR(percpu_kstacks[i]), PTE_W);
 
 }
 
@@ -320,6 +322,10 @@ page_init(void)
   pages[0].pp_link = NULL;
 	size_t i;
 	for (i = 1; i < npages_basemem; i++) {
+    if(i*PGSIZE==MPENTRY_PADDR){
+      pages[i].pp_link=NULL;
+      continue;
+    }
 		pages[i].pp_ref = 0;
 		pages[i].pp_link = page_free_list;
 		page_free_list = &pages[i];
@@ -604,7 +610,13 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+  uintptr_t ret=base;
+  size=ROUNDUP(size, PGSIZE);
+  boot_map_region(kern_pgdir, base, size, pa, PTE_W|PTE_PCD|PTE_PWT);
+  base+=size;
+  if(base>MMIOLIM)
+    panic("mmio_map_region overflow\n");
+  return (void*)ret;
 }
 
 static uintptr_t user_mem_check_addr;
--- kern/trap.c
+++ kern/trap.c
@@ -113,23 +113,37 @@ trap_init_percpu(void)
 	//
 	// LAB 4: Your code here:
 
-	// Setup a TSS so that we get the right stack
-	// when we trap to the kernel.
-	ts.ts_esp0 = KSTACKTOP;
-	ts.ts_ss0 = GD_KD;
-	ts.ts_iomb = sizeof(struct Taskstate);
+  for(int i=0;i<NCPU;i++){
+    struct Taskstate *ts=&(thiscpu->cpu_ts);
+    ts->ts_esp0 = KSTACKTOP-i*(KSTKSIZE+KSTKGAP)-KSTKSIZE;
+    ts->ts_ss0 = GD_KD;
+    ts->ts_iomb = sizeof(struct Taskstate);
 
-	// Initialize the TSS slot of the gdt.
-	gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
-					sizeof(struct Taskstate) - 1, 0);
-	gdt[GD_TSS0 >> 3].sd_s = 0;
+    gdt[(GD_TSS0 >> 3) + i] = SEG16(STS_T32A, (uint32_t) ts,
+            sizeof(struct Taskstate) - 1, 0);
+    gdt[(GD_TSS0 >> 3) + i].sd_s = 0;
+  }
 
-	// Load the TSS selector (like other segment selectors, the
-	// bottom three bits are special; we leave them 0)
-	ltr(GD_TSS0);
+  ltr(GD_TSS0);
+  lidt(&idt_pd);
 
-	// Load the IDT
-	lidt(&idt_pd);
+// 	// Setup a TSS so that we get the right stack
+// 	// when we trap to the kernel.
+// 	ts.ts_esp0 = KSTACKTOP;
+// 	ts.ts_ss0 = GD_KD;
+// 	ts.ts_iomb = sizeof(struct Taskstate);
+// 
+// 	// Initialize the TSS slot of the gdt.
+// 	gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
+// 					sizeof(struct Taskstate) - 1, 0);
+// 	gdt[GD_TSS0 >> 3].sd_s = 0;
+// 
+// 	// Load the TSS selector (like other segment selectors, the
+// 	// bottom three bits are special; we leave them 0)
+// 	ltr(GD_TSS0);
+// 
+// 	// Load the IDT
+// 	lidt(&idt_pd);
 }
 
 void
